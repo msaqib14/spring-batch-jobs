7250ebe08f7ebf26e87bdd2ae15c76c0f0ff0f80
Mohd Saqib <116734915+msaqib14@users.noreply.github.com>
2024-09-24 18:47:11 +0530

    project:added

---
+++ batch-jobs/src/main/java/com/spring_batch_jobs/BatchJobsApplication.java
@@ -0,0 +1,13 @@
+package com.spring_batch_jobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchJobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchJobsApplication.class, args);
+	}
+
+}
---
+++ batch-jobs/src/test/java/com/spring_batch_jobs/BatchJobsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_batch_jobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchJobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
